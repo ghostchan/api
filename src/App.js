f1fc53dcd2b5a7b629c2676be6d5a53476802ee5
zhi.chen <zhi.chen@ucarinc.com>
2017-11-14 10:49:23 +0800

    layout

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Layout} from 'antd';
+import Dialog from './components/Dialog'
+import Tables from './components/Tables'
+const { Header, Content, Footer } = Layout;
 
-class App extends Component {
-  render() {
+class App extends React.Component {
+  render(){
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+      <Layout>
+      <Header style={{ position: 'fixed', width: '100%' }}>
+        <Dialog/>
+      </Header>
+      <Content style={{ padding: '0 50px', marginTop: 64 }}>
+      
+        <Tables/>
+      </Content>
+      <Footer style={{ textAlign: 'center' }}>
+        Jinn Design ©2018 Created by Jinn
+      </Footer>
+    </Layout>
+    )
   }
 }
-
 export default App;
