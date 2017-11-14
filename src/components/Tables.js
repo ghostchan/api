9212ddc352a13709d5af2d34317960430192eee9
zhi.chen <zhi.chen@ucarinc.com>
2017-11-14 17:12:17 +0800

    修改删除

--- src/components/Tables.js
+++ src/components/Tables.js
@@ -1,49 +1,73 @@
-import React, { Component } from 'react';
-import { Table, Icon,Button } from 'antd';
-import Modal from './myModal';
+import React, { Component } from "react";
+import { Table, Icon, Button } from "antd";
+import Modal from "./myModal";
 
-
-const columns = [{
-  title: 'API地址',
-  dataIndex: 'api',
-  key: 'api',
-  render: text => text,
-}, 
- {
-  title: '操作',
-  key: 'action',
-  render: (text, record) => (
-    <span>
-      <Button type="primary" onClick={this.showModal}>修改</Button>&nbsp;
-      <Button type="primary" onClick={this.showModal}>删除</Button>
-    </span>
-  )
-}];
-
-const data = [{
-  key: '1',
-  api: '/api/manage/listInfo'
-}, {
-  key: '2',
-  api: '/api/manage/order/priceList'
-}, {
-  key: '3',
-  api: '/api/manage/company/invoice/delete'
-}];
 class Tables extends React.Component {
-  showModal=()=>{
-    console.log("8888");
-    this.refs.mymodal.toggleShow();
-    this.setState({title:"修改"});
-  }
-    render(){
-        return (
-          <div>
-             <Table columns={columns} dataSource={data} bordered={true}/> 
-             <Modal ref='mymodal' title="修改" visible={false}></Modal>
-          </div>
-           
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: [
+        {
+          key: "1",
+          api: "/api/manage/listInfo"
+        },
+        {
+          key: "2",
+          api: "/api/manage/order/priceList"
+        },
+        {
+          key: "3",
+          api: "/api/manage/company/invoice/delete"
+        }
+      ]
+    };
+    this.columns = [
+      {
+        title: "API地址",
+        dataIndex: "api",
+        key: "api"
+      },
+      {
+        title: "操作",
+        key: "action",
+        render: (text, record) => (
+          <span>
+            <Button
+              type="primary"
+              onClick={this.showModal.bind(this, record.key)}
+            >
+              修改
+            </Button>&nbsp;
+            <Button
+              type="primary"
+              onClick={this.delete.bind(this, record.key)}
+            >
+              删除
+            </Button>
+          </span>
         )
-    }
+      }
+    ];
+  }
+  showModal = key => {
+    console.log(key);
+    this.refs.mymodal.toggleShow();
+    this.setState({ title: "修改" });
+  };
+  delete = key => {
+    console.log("删除："+key);
+  }
+  render() {
+    return (
+      <div>
+        <Table
+          columns={this.columns}
+          dataSource={this.state.data}
+          bordered={true}
+        />
+        <Modal ref="mymodal" title="修改" visible={false} />
+      </div>
+    );
+  }
 }
 export default Tables;
